97b8fcda75d6cc8f6d55e72e313f8282a0f01591
naikmonika012 <naikmonika012@gmail.com>
2023-03-25 14:26:06 +0530

    added allow host

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3p_sr2(7^8k_m$qae$rz$h*hfj=&wcid8$_o+(d+$8v^jcydcg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
